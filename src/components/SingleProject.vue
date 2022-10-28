3692807299d7f43cea6654e96edd4cf3d894738e
MYO-THEIN <okmt21@gmail.com>
2022-10-28 19:48:48 +0630

    L 1, 2, 3

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="project">
+        <div class="flexing">
+            <div>
+                <h3 @click="showDetail = !showDetail">{{project.title}}</h3>
+            </div>
+            <div>
+                <span class="material-icons">done</span>
+                <span class="material-icons">edit</span>
+                <span class="material-icons">delete</span>
+            </div>
+        </div>
+        <p v-if="showDetail">{{project.detail}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showDetail: false
+        }
+    },
+    props: [
+        'project'
+    ]
+}
+</script>
+
+<style>
+.flexing {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.project {
+    background-color: #F2F2F2;
+    padding: 20px;
+    margin: 10px;
+    border-left: 5px solid crimson;
+    border-radius: 10px;
+}
+h3 {
+    color: indigo;
+    cursor: pointer; 
+}
+span {
+    margin-left: 3px;
+}
+span:hover {
+    color: blue;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SingleProject from '@/components/SingleProject.vue';
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    SingleProject
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      this.projects = data;
+    })
+    .catch((error)=>{
+      console.log(error.message);    
+    });
   }
 }
 </script>
+
+<style scoped>
+
+</style>
